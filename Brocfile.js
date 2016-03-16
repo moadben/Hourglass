20738d0fa2d8209d24029b7b6a16a7c1d0504bc6
Moad Ben-Suleiman <moadben95@gmail.com>
2016-03-16 16:48:43 -0700

    First commit

--- Brocfile.js
+++ Brocfile.js
@@ -1,7 +1,4 @@
-var mergeTrees = require('broccoli-merge-trees');
-var fastBrowserify = require('broccoli-fast-browserify');
 var babelTranspiler = require("broccoli-babel-transpiler");
-var babelify = require('babelify');
 
 
 var scriptTree = babelTranspiler('js');
