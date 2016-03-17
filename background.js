6d327f064bb2a7ed5fa5b63cb6f224245e0f176c
Moad Ben-Suleiman <moadben95@gmail.com>
2016-03-16 18:20:01 -0700

    Time working wooooo

--- background.js
+++ background.js
@@ -1,25 +1,28 @@
 var times = [];
-
+var activeTab;
+var currentDomain;
 chrome.tabs.onSelectionChanged.addListener(function(tab) {
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
+    activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed", "times": times});
   });
 });
 
 chrome.tabs.onUpdated.addListener(function(tab) {
+  updateTime();
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
+    activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed", "times": times});
   });
 });
 
 chrome.tabs.onRemoved.addListener(function(tab) {
+  updateTime();
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
+  activeTab = tabs[0];
   });
 });
 
@@ -27,7 +30,21 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "track_tab" ) {
       times = request.times;
-      alert(times);
+      currentDomain = request.url;
+      alert(request.url);
     }
   }
 );
+
+function updateTime(){
+  if(times.length == 0){
+    return;
+  }
+  for(i = 0; i<times.length; i++){
+        if(times[i].webname == currentDomain){
+          times[i].total_time = (new Date().getTime()) - times[i].start_time;
+          console.log(times[i].total_time/1000);
+          break;
+        }
+      }
+}
--- myscript.js
+++ myscript.js
@@ -5,13 +5,17 @@ chrome.runtime.onMessage.addListener(
       var isIn = 0;
       var firstHref = window.location.hostname;
       for(i = 0; i<request.times.length; i++){
-      	if(request.times[i] == firstHref){
+      	if(request.times[i].webname == firstHref){
       		isIn = 1;
       		break;
       	}
       }
       if(isIn == 0){
-      	request.times.push(firstHref);
+        var website = {
+          webname: firstHref,
+          start_time: new Date().getTime(),
+          total_time: 0 };
+      	request.times.push(website);
       }
 
       //console.log(firstHref);
