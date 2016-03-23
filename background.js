c6316ee35cd204ad1c863a96c40e944105c672e0
Moad Ben-Suleiman <moadben95@gmail.com>
2016-03-23 14:17:20 -0700

    Fuck me.

--- background.js
+++ background.js
@@ -2,42 +2,57 @@ var times = [];
 var activeTab;
 var currentDomain;
 
-/*chrome.browserAction.onClicked.addListener(function(tab){
- chrome.app.window.create('popup.html', {
-    'outerBounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
-});*/
-
 function backgroundfunction(){
   updateTime();
   return times;
 }
 
-chrome.tabs.onSelectionChanged.addListener(function(tab) {
-
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed", "times": times});
-  });
-});
 
 chrome.tabs.onUpdated.addListener(function(tab) {
   updateTime();
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({active: true, currentWindow: true, 'lastFocusedWindow': true}, function(tabs) {
     activeTab = tabs[0];
+    while(!activeTab.id){
+      initTime();
+    }
     chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed", "times": times});
+  });
+});
+
+chrome.tabs.onActivated.addListener(function(tab) {
+  updateTime();
+
+  chrome.tabs.query({active: true, currentWindow: true, 'lastFocusedWindow': true}, function(tabs) {
+    activeTab = tabs[0];
+     while(!activeTab.id){
+      initTime();
+    }
+     chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed", "times": times});
+  });
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  updateTime();
+
+  chrome.tabs.query({active: true, currentWindow: true, 'lastFocusedWindow': true}, function(tabs) {
+    activeTab = tabs[0];
+     while(!activeTab.id){
+      initTime();
+    }
+     chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed", "times": times});
   });
 });
 
 chrome.tabs.onRemoved.addListener(function(tab) {
   updateTime();
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({active: true, currentWindow: true, 'lastFocusedWindow': true}, function(tabs) {
   activeTab = tabs[0];
+  while(!activeTab.id){
+      initTime();
+    }
+   // chrome.tabs.sendMessage(activeTab.id, {"message": "tab_changed", "times": times});
   });
 });
 
@@ -46,7 +61,8 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "track_tab" ) {
       times = request.times;
       currentDomain = request.url;
-      //alert(request.url);
+      // alert(request.url);
+      return true;
     }
   }
 );
@@ -66,8 +82,16 @@ function updateTime(){
       }
 }
 
-var newHTML = [];
-for (var i = 0; i < times.length; i++) {
-    newHTML.push('<span>' + times[i].webname +": " + times[i].total_time + '</span>');
+function initTime(){
+  if(times.length == 0){
+    return;
+  }
+  for(i = 0; i<times.length; i++){
+        if(times[i].webname == currentDomain){
+          times[i].start_time = new Date().getTime();
+          var cool = (times[i].webname + ": " + times[i].total_time/1000);
+          console.log("Yeah");
+          return;
+        }
+      }
 }
-$(".curr").html(newHTML.join(""));
--- myscript.js
+++ myscript.js
@@ -1,14 +1,14 @@
 // content.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "tab_changed" ) {
+     if( request.message === "tab_changed" ) {
       var isIn = 0;
       var firstHref = window.location.hostname;
       for(i = 0; i<request.times.length; i++){
       	if(request.times[i].webname == firstHref){
-          times[i].start_time = new Date().getTime()
-      		isIn = 1;
-      		break;
+          request.times[i].start_time = new Date().getTime();
+          isIn = 1;
+          // alert("I know I've been got a long time bot");
       	}
       }
       if(isIn == 0){
@@ -22,7 +22,8 @@ chrome.runtime.onMessage.addListener(
 
       //console.log(firstHref);
       console.log("Hello");
+      // alert("I'm back and I want what is mine");
       chrome.runtime.sendMessage({"message": "track_tab", "url": firstHref, "times":request.times});
-    }
   }
+   }
 );
