c290da06facb700200a807c177e8bd980baaecd6
Moad Ben-Suleiman <moadben95@gmail.com>
2016-03-23 14:25:31 -0700

    Darn dood

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
     var times = data.backgroundfunction();
     console.log(times);
     var newHTML = [];
+    newHTML.push('<ul>');
 	for (var i = 0; i < times.length; i++) {
-		newHTML.push('<ul>');
-	    newHTML.push('<il>' + '<img src=' + times[i].favicon + '/>' + times[i].webname +": " + times[i].total_time/1000 + '</il>');
-	    newHTML.push('</ul');
+	    newHTML.push('<li style=\"border-bottom:1px, solid, black\"">' + '<img src=' + times[i].favicon + '/>' + times[i].webname +": " + times[i].total_time/1000 + '</li>');
 	}
+	newHTML.push('</ul>')
 	$("div.curr").html(newHTML);
 })();
